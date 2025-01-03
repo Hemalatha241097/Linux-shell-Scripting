562a2d5f25b6ca869d8ef29433f58f9885aaaa98
hema <attotihemalatha08@gmail.com>
2025-01-03 06:36:25 +0530

    masked username and password

--- userandpassword.sh
+++ userandpassword.sh
@@ -4,6 +4,10 @@ echo -n "Enter your username : "
 
 read -s USERNAME #masked username
 
+echo -n "Here is yourusername :  $USERNAME"
+
 echo -n "Enter your password : "
 
 read -s PASSWORD #masked password for security purpose
+
+echo -n "It is your password :  $PASSWORD"
