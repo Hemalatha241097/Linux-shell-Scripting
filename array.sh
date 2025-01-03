3a3ce9c3ad39be433e2e32d967212229b1fb0877
hema <attotihemalatha08@gmail.com>
2025-01-03 18:43:29 +0530

    array implementation

--- array.sh
+++ array.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
-STAFF_NAMES=(Hema, Mahesh, Aadhya)
+STAFF_NAMES=(Hema Mahesh Aadhya)
 
 echo "First Person : ${STAFF_NAMES[0]}"
+
+echo "Second Person : ${STAFF_NAMES[1]}"
+
+echo "Print all the staff members : ${STAFF_NAMES[@]}"
+
