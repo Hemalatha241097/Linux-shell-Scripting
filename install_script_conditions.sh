fe438052d798eed12260eae82134303d12a2d8d8
hema <attotihemalatha08@gmail.com>
2025-01-03 19:21:24 +0530

    installing sql server

--- install_script_conditions.sh
+++ install_script_conditions.sh
@@ -3,4 +3,13 @@
 #installation of mysql and git by cheching whether they are already installed or not. Also check the installation is successful or not
 
 echo "Installing sql server..."
-dnf install mysql-server -y
+
+USER_ID=$(id -u)
+
+if [ $USER_ID -ne 0 ]
+    then
+    echo "You should use root to install sql server"
+    exit 1
+else
+    dnf install mysql-server -y
+fi
