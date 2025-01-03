85869476780990c2aba928782cf33e120da76d78
hema <attotihemalatha08@gmail.com>
2025-01-03 19:18:27 +0530

    installing sql server

---
+++ install_script_conditions.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+#installation of mysql and git by cheching whether they are already installed or not. Also check the installation is successful or not
+
+echo "Installing sql server..."
+dnf install mysql-server -y
