d28cba43434b5c9e07587380a61cdb2ae0d947ee
hema <attotihemalatha08@gmail.com>
2025-01-04 22:21:35 +0530

    delete log files less than 24hours

---
+++ delete_old_logs.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+SOURCE_DIR="/home/ec2-user/log-files.log"
+
+LOGS_FOLDER="/var/log/shellscript-logs"
+LOG_FILE=$(echo $0 | cut -d "." -f1 )
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
+
+
+CHECK_ROOT(){
+    if [ $USERID -ne 0 ]
+    then
+        echo "ERROR:: You must have sudo access to execute this script"
+        exit 1 #other than 0
+    fi
+}
+
+echo "Script started executing at: $TIMESTAMP" &>>$LOG_FILE_NAME
+
+CHECK_ROOT
+
+FILES_TO_DELETE=$(find $SOURCE_DIR -name "*.log" -mtime -1)
+echo "Files to be deleted: $FILES_TO_DELETE"
+
+while read -r filepath
+do
+    echo "Deleting file: $filepath" &>>$LOG_FILE_NAME
+    rm -rf $filepath
+    echo "Deleted file: $filepath"
+done <<< $FILES_TO_DELETE
--- install_multiple_packages.sh
+++ install_multiple_packages.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+USER_ID=$(id -u)
+
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
@@ -12,19 +14,12 @@ LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
 
 CHECK_ROOT(){
 
-    USER_ID=$(id -u)
-
     if [ $USER_ID -ne 0 ]
     then
         echo "ERROR : You must have root access to install this script"
         exit 1
     fi
 }
-
-
-echo "Script started executing at: $TIMESTAMP" &>>$LOG_FILE_NAME
-
-CHECK_ROOT
 
 VALIDATE(){
     if [ $1 -ne 0 ]
@@ -35,6 +30,10 @@ VALIDATE(){
              echo -e "$2...$G SUCCESS $N"
     fi
 }
+
+echo "Script started executing at: $TIMESTAMP" &>>$LOG_FILE_NAME
+
+CHECK_ROOT
 
 for package in $@
 do
