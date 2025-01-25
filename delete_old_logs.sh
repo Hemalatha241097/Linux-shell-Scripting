cfbe2dfb5a7a7f950ccbb77d4d126757de801938
hema <attotihemalatha08@gmail.com>
2025-01-25 07:38:19 +0530

    backup files older than 14 days

--- delete_old_logs.sh
+++ delete_old_logs.sh
@@ -26,7 +26,7 @@ echo "Script started executing at: $TIMESTAMP" &>>$LOG_FILE_NAME
 
 CHECK_ROOT
 
-FILES_TO_DELETE=$(find $SOURCE_DIR -name "*.log" -mtime +14)
+FILES_TO_DELETE=$(find $SOURCE_DIR -name "*.log" -mtime -1)
 echo "Files to be deleted: $FILES_TO_DELETE"
 
 while read -r filepath
